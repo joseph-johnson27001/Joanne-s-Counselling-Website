67d9dd1676d016502541fac0b7f7513d7eb0c556
Joe Johnson <joseph.johnson27001@gmail.com>
2026-05-07 19:03:22 +0700

    Updated about and hero

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -13,51 +13,31 @@
       <div class="about-grid">
         <div class="about-copy">
           <p>
-            A couple of students recently interviewed me for their GCSE
-            assignment. One of them asked if I always knew I wanted to study
-            psychology. Right away, my response was "I didn't!". Growing up in
-            an Asian household, for me back then, meant balancing what my
-            parents hoped for me (Ahem! medical school) with what I wanted for
-            myself. At the time, (and crossing my fingers that I wasn't the only
-            one) I didn't really have a clear answer. But what I later realized
-            is how much I value human connection.
+            My counselling journey may have began in the Philippines but it has since taken me across borders and cultures. I have lived, studied, and worked in both the Philippines and Thailand, gaining experience in youth support, education, and mental health. For the past 9 years, I have made Bangkok my home, where I've worked in international schools supporting children, adolescents and families from diverse backgrounds that continue to shape my approach to counselling.
           </p>
           <p>
-            This became clear while I was undertaking my Bachelor of Science in
-            Psychology as an intern in a youth reception centre where I
-            supported young people through assessments, facilitated group
-            activities, and worked alongside a team to address emotional and
-            behavioural needs. After a natural calamity in the Philippines, I
-            trained as a Mental Health First Aid provider to volunteer and offer
-            my support. These experiences gave me the chance to put theory into
-            practice and helped clarify my path forward.
+            It all started during my final year of University where I interned at a youth reception centre, supporting young people through collaborating with a multidisciplinary team to address emotional and behavioural needs. Following a natural calamity, which is quite common in the Philippines, I trained as a Mental Health First Aid provider and volunteered in crisis support, which became a meaningful first step for me to put theory into practice.
           </p>
           <p>
-            Months later, as I was preparing to apply for my masters, life threw
-            a curve ball and I have made the decision to move to Bangkok to
-            support my sister through a health crisis. During that time, I
-            started teaching as a kindergarten teacher, which gave me first-hand
-            experience in child development and social-emotional learning.
-            Working with young children further deepened my understanding how
-            behaviour is a form of communication. This insight inspired me to
-            pursue a certification in Therapeutic Play skills, enhancing my
-            ability to connect with and support children in meaningful ways,
-            meeting them where they are.
+            Life eventually brought me to Bangkok, where I initially worked as a kindergarten teacher. This deepened my understanding of child development and inspired me to pursue Therapeutic Play Skills training alongside my Master of Science in Counselling Psychology.
           </p>
           <p>
-            By then, with a Master of Science in Counselling Psychology, I have
-            gained experience working with individuals online and later in
-            person as a school counsellor in international schools. These roles
-            (moving to a new country, changes in careers and a global pandemic
-            later) have allowed me to support clients from a wide range of
-            cultures, backgrounds, and walks of life.
+            I have since worked as a school counsellor in international schools, supporting students from early years through adolescence, as well as families and staff. During COVID, I also provided online counselling for adults, adapting my approach to maintain meaningful connection and continuity of care during uncertain times. My work has included individual and group counselling, wellbeing programmes and intervention development, and serving as a Designated Safeguarding Lead, always with a focus on holistic, culturally responsive care.
           </p>
           <p>
-            I've learned to appreciate diversity and the unique strengths and
-            stories each person brings into the counselling room. This lead me
-            to the counsellor I am today, committed to creating a warm,
-            non-judgmental space where children, adolescents, and adults can
-            feel heard, supported, and empowered to grow.
+            My approach is integrative, drawing from person-centered, play-based, and evidence-informed modalities. I have supported clients through transitions, cross-cultural challenges, grief, trauma, relationship changes, and periods of feeling disconnected or searching for meaning.
+          </p>
+          <p>
+            I've learned to appreciate the unique strengths and stories each person brings into my counselling room. This lead me to the counsellor I am today, committed to creating a warm, non-judgmental space where children, adolescents, and adults can feel heard, supported, and empowered to grow.
+          </p>
+          <p>
+            I believe in ongoing professional development and adhere to the ethical standards of my affiliated organizations, which you can view here:
+          </p>
+          <p>
+            <a href="https://drive.google.com/file/d/1PnoG0H1E7hb4leTSO0QpEF3wYPFY6gTM/view?pli=1" target="_blank">ASCA Ethical Standards</a>
+          </p>
+          <p>
+            <a href="https://playtherapy.org.uk/wp-content/uploads/2025/08/PTUK-Ethical-Framework.pdf" target="_blank">Play Therapy UK Ethical Framework</a>
           </p>
         </div>
 
